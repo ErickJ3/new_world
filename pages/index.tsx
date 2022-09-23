ce65744edb49c8eaa01000efa099f623917f9aa9
Erick Jesus <erick.jesus2060@gmail.com>
2022-09-23 10:56:09 -0300

    animation: testing

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,29 @@
+import { useEffect } from "react";
 import type { NextPage } from "next";
 import styles from "../styles/Home.module.css";
 import { Canvas } from "@react-three/fiber";
-import { OrbitControls, Stats } from "@react-three/drei";
+import {
+  OrbitControls,
+  Stats,
+  useAnimations,
+  useGLTF,
+} from "@react-three/drei";
 
 import Lights from "../components/Lights";
 import Ground from "../components/Ground";
-import Trees from "../components/Trees";
+
+const MyCube = () => {
+  const model = useGLTF("./models/cube.glb");
+  const { actions } = useAnimations(model.animations, model.scene);
+
+  console.log(model);
+
+  useEffect(() => {
+    actions?.defeat?.play();
+  }, []);
+
+  return <primitive object={model.scene} />;
+};
 
 const Home: NextPage = () => {
   const testing = true;
@@ -18,7 +36,7 @@ const Home: NextPage = () => {
         <gridHelper args={[100, 100]} />
         <OrbitControls />
         <Lights />
-        <Trees boundary={100} count={50} />
+        <MyCube />
         <Ground />
       </Canvas>
     </div>
